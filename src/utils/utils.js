4a840c1037a644ba467daee3dc0edf1ebbbc438e
qui.ngo <qui.ngo@finviet.com.vn>
2023-04-24 19:32:53 +0700

    sửa kết quả khảo s

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -24,37 +24,37 @@ export const getResult = (key, score) => {
       if (score === 33)
         return "Biểu hiện lo âu trong học tập của bạn ở mức trung bình";
       if (score <= 24.11)
-        return "Bạn ít có khả năng có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn ít có khả năng có biểu hiện lo âu liên quan đến việc học";
       if (score <= 27.14)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức rất thấp";
+        return "Biểu hiện lo âu liên quan đến việc học của bạn ở mức rất thấp";
       if (score <= 33.2)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức thấp";
+        return "Biểu hiện lo âu liên quan đến việc học của bạn ở mức thấp";
       if (score <= 39.26)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức vừa phải";
+        return "Biểu hiện lo âu liên quan đến việc học của bạn ở mức vừa phải";
       if (score <= 42.29)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức khá cao";
+        return "Biểu hiện lo âu liên quan đến việc học của bạn ở mức khá cao";
       if (score <= 45.32)
-        return "Bạn có nguy cơ cao có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn có nguy cơ cao có biểu hiện lo âu liên quan đến việc học";
       if (score > 45.32)
-        return "Bạn có nguy cơ rất cao có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn có nguy cơ rất cao có biểu hiện lo âu liên quan đến việc học";
     }
     case QUESTION_SCREEN.THI_CU: {
       if (score === 38)
         return "Biểu hiện lo âu trong học tập của bạn ở mức trung bình";
       if (score <= 27.14)
-        return "Bạn ít có khả năng có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn ít có khả năng có biểu hiện lo âu liên quan đến thi cử";
       if (score <= 30.81)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức rất thấp";
+        return "Biểu hiện lo âu liên quan đến thi cử của bạn ở mức rất thấp";
       if (score <= 38.15)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức thấp";
+        return "Biểu hiện lo âu liên quan đến thi cử của bạn ở mức thấp";
       if (score <= 45.49)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức vừa phải";
+        return "Biểu hiện lo âu liên quan đến thi cử của bạn ở mức vừa phải";
       if (score <= 49.16)
-        return "Biểu hiện lo âu liên quan đến lớp học của bạn ở mức khá cao";
+        return "Biểu hiện lo âu liên quan đến thi cử của bạn ở mức khá cao";
       if (score <= 52.83)
-        return "Bạn có nguy cơ cao có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn có nguy cơ cao có biểu hiện lo âu liên quan đến thi cử";
       if (score > 52.83)
-        return "Bạn có nguy cơ rất cao có biểu hiện lo âu liên quan đến lớp học";
+        return "Bạn có nguy cơ rất cao có biểu hiện lo âu liên quan đến thi cử";
     }
   }
 };
